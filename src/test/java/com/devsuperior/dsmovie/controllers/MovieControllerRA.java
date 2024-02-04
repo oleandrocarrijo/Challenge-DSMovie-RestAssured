1636dd1cae74ebb33c54bda4eca454b09c8025ba
Leandro Carrijo <7leandrobatista@gmail.com>
2024-02-04 11:37:39 -0300

    feat: findAllShouldReturnOkWhenMovieNoArgumentsGiven

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -3,10 +3,25 @@ package com.devsuperior.dsmovie.controllers;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static  org.hamcrest.Matchers.*;
+
 public class MovieControllerRA {
-	
+
+	@Test
+	void setup() throws JSONException{
+		baseURI = "http://localhost:8080";
+	}
+
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+
+		given()
+				.get("/movies?page=0")
+				.then()
+				.statusCode(200)
+				.body("content.title", hasItems("The Witcher", "Titanic", "Django Livre"));
 	}
 	
 	@Test
