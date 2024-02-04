ff8068c58103929092d861720fad1eb6603497bf
Leandro Carrijo <7leandrobatista@gmail.com>
2024-02-04 11:50:25 -0300

    feat: findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,17 +1,24 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static  org.hamcrest.Matchers.*;
 
 public class MovieControllerRA {
 
-	@Test
+	private String movieName;
+
+	@BeforeEach
 	void setup() throws JSONException{
 		baseURI = "http://localhost:8080";
+
+		movieName = "Matrix Resurrections";
 	}
 
 	@Test
@@ -25,7 +32,17 @@ public class MovieControllerRA {
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+
+		given()
+				.get("/movies?title={movieName}", movieName)
+				.then()
+				.statusCode(200)
+				.body("content.id[0]", is(4))
+				.body("content.title[0]", equalTo("Matrix Resurrections"))
+				.body("content.score[0]", is(0.0F))
+				.body("content.count[0]", is(0))
+				.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/hv7o3VgfsairBoQFAawgaQ4cR1m.jpg"));
 	}
 	
 	@Test
